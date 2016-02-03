2d97b9b1fad7527b0f25399f345c3d609fcad31e
Matthew Adams <matthewcodes@gmail.com>
2016-02-03 09:49:50 -0500

    create initial interpolator and test

---
+++ Interpolator.java
@@ -0,0 +1,43 @@
+package examples;
+
+public class Interpolator {
+
+	/**
+	 * Returns the y-value of a point interpolated between two points.
+	 * 
+	 * @param point1 a point [x,y]
+	 * @param point2 another point [x,y]
+	 * @param x the x-value of the interpolated point
+	 * @return the y-value of the interpolated point
+	 */
+	public double linearInterpolate(double[] point1, double[] point2, double x) {
+		double x1 = point1[0];
+		double y1 = point1[1];
+		double x2 = point2[0];
+		double y2 = point2[1];
+		
+		double y = y1 + (y2-y1)*(x-x1)/(x2-x1);
+		return y;
+	}
+	
+	
+	/**
+	 * Returns the midpoint between the two points
+	 * 
+	 * @param point1 a point [x,y]
+	 * @param point2 another point [x,y]
+	 * @return the midpoint [x,y]
+	 */
+	public double[] getMidpoint(double[] point1, double[] point2) {
+		double x1 = point1[0];
+		double y1 = point1[1];
+		double x2 = point2[0];
+		double y2 = point2[1];
+		
+		double x = (x1+x2)/2;
+		double y = (y1+y2)/2;
+		double[] midpoint = {x,y};
+		
+		return midpoint;
+	}
+}
---
+++ InterpolatorTest.java
@@ -0,0 +1,39 @@
+package examples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InterpolatorTest {
+	Interpolator in;
+	double doublePrecision;
+	
+	@Before
+	public void setUp() throws Exception {
+		in = new Interpolator();
+		doublePrecision = 0.0001;
+	}
+
+	
+	@Test
+	public void testLinearInterpolate() {
+		double[] p1 = {-1,-1};
+		double[] p2 = {3,3};
+		
+		double y = in.linearInterpolate(p1, p2, 1.4);
+		assertEquals(1.4, y, doublePrecision);
+	}
+
+	
+	@Test
+	public void testGetMidpoint() {
+		double[] p1 = {-1,-1};
+		double[] p2 = {3,3};
+		
+		double[] midptActual = in.getMidpoint(p1, p2);
+		double[] midptExpected = {1.0,1.0};
+		assertArrayEquals(midptExpected, midptActual, doublePrecision);
+	}
+
+}
